cf01b5e5c50ad66b8598b4ecbfb9c57f19c7f497
yeminko <yeminko47@gmail.com>
2025-10-17 17:29:40 +0630

    Refactor app.js to remove commented-out code and ensure proper MongoDB connection setup

--- 04-01-networking-containers/app.js
+++ 04-01-networking-containers/app.js
@@ -67,17 +67,14 @@ app.get("/people", async (req, res) => {
   }
 });
 
-// For temp testing purposes
-app.listen(3000);
-
-// mongoose.connect(
-//   "mongodb://localhost:27017/swfavorites",
-//   { useNewUrlParser: true },
-//   (err) => {
-//     if (err) {
-//       console.log(err);
-//     } else {
-//       app.listen(3000);
-//     }
-//   }
-// );
+mongoose.connect(
+  "mongodb://172.17.0.2:27017/swfavorites",
+  { useNewUrlParser: true },
+  (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      app.listen(3000);
+    }
+  }
+);
