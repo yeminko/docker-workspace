61129b6ea472a53b9ecad7d152daa1585b24b748
yeminko <yeminko47@gmail.com>
2026-01-14 19:38:40 +0630

    fix: standardize quotation marks and update response message in app.js

--- 12-01-kubernetes-deploy-app/app.js
+++ 12-01-kubernetes-deploy-app/app.js
@@ -1,15 +1,16 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.send(`
-    <h1>Hello from this NodeJS app!</h1>
+    <h1>Hello from this NodeJS app!!!!!!!!!!!!!!!</h1>
+    <p>This is new</p>
     <p>Try sending a request to /error and see what happens</p>
   `);
 });
 
-app.get('/error', (req, res) => {
+app.get("/error", (req, res) => {
   process.exit(1);
 });
 
