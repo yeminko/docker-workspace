b361b8d88b1e740c439aa59c85bb5c6e9ed976e1
yeminko <yeminko47@gmail.com>
2026-07-22 08:58:48 +0630

    fix: update fetch URL and response message format

--- 16-docker-test/caller-nodejs/index.js
+++ 16-docker-test/caller-nodejs/index.js
@@ -2,11 +2,11 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  // fetch("http://localhost:3000")
-  fetch("http://host.docker.internal:3000")
+  fetch("http://my-app:3000")
+    // fetch("http://host.docker.internal:3000")
     .then((response) => response.text())
     .then((data) => {
-      res.send("Fetched data: " + data);
+      res.send("Fetched data!!!!: " + data);
     })
     .catch((error) => {
       console.error("Error fetching data:", error);
