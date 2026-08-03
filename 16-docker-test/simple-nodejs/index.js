bb7c5abca3915d71e5095c9f5e58571ec48d8077
yeminko <yeminko47@gmail.com>
2026-08-03 08:34:46 +0630

    feat: use environment variable for port configuration in Dockerfile and index.js

--- 16-docker-test/simple-nodejs/index.js
+++ 16-docker-test/simple-nodejs/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
 
+const PORT = process.env.PORT;
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(3000, () => {
-  console.log("Server running at http://localhost:3000");
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
 });
